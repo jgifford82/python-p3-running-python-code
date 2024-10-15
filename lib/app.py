0ea8c1e12bac4989cfa18e943ad2bb6ede044bae
MacIntel <macintel@MacIntels-MacBook-Pro.local>
2024-10-15 13:19:15 -0600

    added comments & solution

---
+++ lib/app.py
@@ -0,0 +1,22 @@
+# print("Hello world!", end=" ")
+# print("Happy fall!", end="!! ")
+# print("Hooray for spooky season!", end="!!!\n :D")
+
+# Execute the file in the terminal to see printed statements by running:
+# python lib/app.py
+
+# to access python shell in terminal to run code, run:
+# python
+
+# then type something like:
+# print("Hello Python shell")
+
+# first_number = 7
+# print(first_number)
+
+# exit the Python shell by typing exit(), or pressing ctrl + d
+
+# to run test, type pytest in terminal or pytest -x to stop running after first failed test
+
+
+print("Hello World! Pass this test, please.")
